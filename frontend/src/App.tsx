7430a554fb96bf48a1c95ca0180f74bd0d3a8c84
Aidan Naidoo <ST10109482@vcconnect.edu.za>
2024-10-07 00:39:27 +0200

    Remove unnecessary files from .gitignore and add package-lock.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/navbar";
-import PostList from "./components/postList";
-// import PostEdit from "./components/postEdit";
-import PostCreate from "./components/postCreate";
 import SignUp from './components/signUp';
 import Login from "./components/login";
 import CustomerDashboard from './components/customerDashboard';
@@ -20,10 +17,8 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/postList" element={<ProtectedRoute><PostList /></ProtectedRoute>} />
         <Route path="/dashboard" element={<ProtectedRoute><CustomerDashboard /></ProtectedRoute>} />
         {/* <Route path="/edit/:id" element={<PostEdit />} /> */}
-        <Route path="/create" element={<ProtectedRoute><PostCreate /></ProtectedRoute>} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/payment" element={<ProtectedRoute><PaymentForm /></ProtectedRoute>} />
