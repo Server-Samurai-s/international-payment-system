0965a7c9d61dbe79867f2112ffd20758233a1201
Dylan Miller <st10029256@vcconnect.edu.za>
2024-10-08 13:00:41 +0200

    Update eslint configuration

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,17 +1,14 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
       sourceType: "module",
-      globals: {
-        ...globals.browser,  
-        ...globals.node,
-      },
+      globals: globals.browser, 
     },
   },
   pluginJs.configs.recommended,
