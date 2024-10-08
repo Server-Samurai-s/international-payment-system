46cc3f05c687f86958073195e734baabc84bf3ae
Dylan Miller <st10029256@vcconnect.edu.za>
2024-10-08 14:35:03 +0200

    Fixed eslin.config.mjs

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,20 +1,12 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import globals from "globals";
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: {
-      sourceType: "module",
-      ecmaVersion: "latest",
-      globals: {
-        ...globals.browser,  // Correct placement of browser globals
-        ...globals.node,     // Add node globals if needed
-      },
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
