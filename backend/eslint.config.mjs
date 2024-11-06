c5c785928015a57ade2e63c2b655bbd2c3a7deec
Dylan Miller <st10029256@vcconnect.edu.za>
2024-11-06 21:28:49 +0200

    Updated

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,19 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  tseslint.configs.recommended,
+  pluginReact.configs.recommended,
   {
     settings: {
       react: {
-        version: "detect",  // Automatically detect React version
+        version: "detect", // Automatically detect React version
       },
     },
   },
--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,21 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { sourceType: "module" },
-    globals: globals.browser,
+    languageOptions: {
+      sourceType: "module",
+      globals: globals.browser,
+    },
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  tseslint.configs.recommended,
+  pluginReact.configs.recommended,
   {
     settings: {
       react: {
-        version: "detect", 
+        version: "detect", // Automatically detect React version
       },
     },
   },
