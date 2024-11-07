23aae5b32b21c1bb185d42bc9f904c7d484e70b9
Dylan Miller <st10029256@vcconnect.edu.za>
2024-11-07 19:45:53 +0200

    Updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
 import './App.css';
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Navbar from "./components/navbar";
 import SignUp from './components/signUp';
 import Login from "./components/login";
@@ -13,21 +10,19 @@ import LandingPage from './components/landingPage';
 
 const App = () => {
   return (
-    <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/dashboard" element={<ProtectedRoute><CustomerDashboard /></ProtectedRoute>} />
-        {/* <Route path="/edit/:id" element={<PostEdit />} /> */}
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/payment" element={<ProtectedRoute><PaymentForm /></ProtectedRoute>} />
-      </Routes>
-    </div>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/dashboard" element={<ProtectedRoute><CustomerDashboard /></ProtectedRoute>} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/payment" element={<ProtectedRoute><PaymentForm /></ProtectedRoute>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
